9043382baf790b858d5bc2a3dda3ded0af2dd7b9
kaya-mustf <106195274+kaya-mustf@users.noreply.github.com>
2022-05-26 13:14:47 +0300

    Add files via upload

---
+++ çalışma.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+from tokenize import Double
+
+ 
+
+def us_alma(taban = int (input("bir sayı giriniz")) , tavan = int (input("bir sayı giriniz"))):
+    sonuc = 1
+    for x in range (tavan):
+        sonuc  *=  taban 
+    return sonuc
+        
+
+print(us_alma())
+
+
+        
+
+
+
+
+
